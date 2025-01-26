6320a8d8cf2ddbb7a4ddb1c1d736f383a5436143
Willis Plummer <willisplummer@gmail.com>
2025-01-26 09:49:32 -0600

    open socket, bind to port

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h> // close
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-  printf("Hello, World!\n");
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket == -1) {
+    perror("could not create socket");
+    exit(EXIT_FAILURE);
+  }
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  server_address.sin_port = htons(PORT);
+
+  if (bind(server_socket, (struct sockaddr *)&server_address,
+           sizeof(server_address)) < 0) {
+    perror("Bind failed");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+  }
+
   return 0;
 }
